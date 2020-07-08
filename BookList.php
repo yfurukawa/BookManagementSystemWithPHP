330951e2c4dafa8439f278a884d7cfde3350bf72
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-07-08 18:52:49 +0900

    書籍一覧表示ページのフロントエンド用ファイルの追加

---
+++ BookList.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="UTF-8">
+        <title>書籍管理</title>
+    </head>
+
+    <body>
+        <div class="pageTitle">
+            書籍一覧
+        </div>
+        <div style="font-size: 18px;">テキスト送信テスト</div>
+        <form name="form1" method="POST" action="view.php">
+            名前：<br>
+            <input type="text" name="onamae"><br>
+            <input type="submit" value="送信">
+        </form>
+    </body>
+</html>
