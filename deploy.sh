d174d2ca4ed0b221a68c4f71e2f0978360810dad
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-07-13 20:09:47 +0900

    ディレクトリ名が間違っていたので修正

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@
 cp index.html /var/www/html/BM/
 cp *.php /var/www/html/BM/
 cp php_libs/class/* /var/www/php_libs/class/
-cp php_libs/init.php /var/wwww/php_libs/
+cp php_libs/init.php /var/www/php_libs/
