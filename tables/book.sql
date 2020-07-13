42f73b558cdea3f835730de3f244bd84dbf1bbc7
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-07-13 20:19:31 +0900

    保管場所への関連が無かったので追加

--- tables/book.sql
+++ tables/book.sql
@@ -5,6 +5,7 @@ CREATE TABLE BookManage.book (
   title varchar(255) NOT NULL,
   publisherId SMALLINT UNSIGNED NOT NULL,
   authorId TINYINT UNSIGNED NOT NULL,
+  roomId TINYINT UNSIGNED,
   CONSTRAINT `fk_publisher`
     FOREIGN KEY (publisherId) REFERENCES publisher(publisherId)
     ON DELETE CASCADE
@@ -12,8 +13,13 @@ CREATE TABLE BookManage.book (
   CONSTRAINT `fk_author`
     FOREIGN KEY (authorId) REFERENCES author(authorId)
     ON DELETE CASCADE
+    ON UPDATE RESTRICT,
+  CONSTRAINT `fk_room`
+    FOREIGN KEY (roomId) REFERENCES room(roomId)
+    ON DELETE CASCADE
     ON UPDATE RESTRICT
  );
 
 CREATE INDEX isbn_index ON BookManage.book(isbn);
+
 
