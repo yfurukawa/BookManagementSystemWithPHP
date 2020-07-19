7bb18a0761d673040ed2b9e4600e64cf6ead61fb
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-07-19 21:58:34 +0900

    出力情報を増やした

--- php_libs/class/BookListController.php
+++ php_libs/class/BookListController.php
@@ -8,7 +8,8 @@ class BookListController {
         $c = new DbConnector();
         $connector = $c->connectDb();
         
-        $sql = "SELECT * FROM book";
+        $sql = "SELECT isbn, title, publisherName, name, roomName FROM book as b, author as a, room as r, publisher as p WHERE b.authorID = 
+        a.authorId and b.roomId = r.roomId and b.publisherId = p.publisherId";
         
         $stmh = $connector->prepare($sql);
         $stmh->execute();
@@ -18,7 +19,7 @@ class BookListController {
         }
         else {
             while($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
-                $pubs = $pubs."<tr><td>".$row['title']."</td><td>".$row['isbn']."</td><td>".$row['publisherName']."</td><td>".$row['authorName']."</td><td>".$row['roomName']."</td></tr>";
+                $pubs = $pubs."<tr><td>".$row['title']."</td><td>".$row['isbn']."</td><td>".$row['publisherName']."</td><td>".$row['name']."</td><td>".$row['roomName']."</td></tr>";
             }
             $contents = '<table border="2"><tbody><tr><th>書籍名称</th><th>ISBN</th><th>出版社</th><th>著者</th><th>保管場所</th></tr>';
             $contents = $contents.$pubs;
