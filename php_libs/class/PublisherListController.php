1d322f0da87c571fcdd17d680462a1efc558209e
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-07-20 21:53:26 +0900

    出版社リストの取得をリポジトリに移動した

--- php_libs/class/PublisherListController.php
+++ php_libs/class/PublisherListController.php
@@ -1,21 +1,25 @@
 <?php
 
-class PublisherListController {
-    private $c = "";
+require_once('/var/www/php_libs/class/repository/PublisherQuery.php');
 
+class PublisherListController {
+    private $publishers = array();
+    private $publisherQuery;
+    private $pubs = NULL;
+    
     public function listPublisher() {
-        $pubs = "";
-        $c = new DbConnector();
-        $connector = $c->connectDb();
-        
-        $sql = "SELECT * FROM publisher";
-        
-        $stmh = $connector->prepare($sql);
-        $stmh->execute();
-        
-        while($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
-                $pubs = $pubs."<tr><td>".$row['publisherName']."</td></tr>";
+        $publisherQuery = new PublisherQuery();
+        $publishers = $publisherQuery->publisherListup();
+
+        if(count($publishers) < 1) {
+            print ("表示する情報がありません。<br>");
         }
+        else {
+            foreach($publishers as $publisher) {
+                $pubs = $pubs."<tr><td>".$publisher['publisherName']."</td></tr>";
+            }
+        }
+
         $contents = '<table border="2"><tbody><tr><th>出版社</th></tr>';
         $contents = $contents.$pubs;
         $contents = $contents."</tbody></table>";
