c7d5a67f84b1de5b2a24bb6bf5fbb3df2fcd142d
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-07-20 21:55:31 +0900

    書籍一覧を取得するクエリクラスの追加

---
+++ php_libs/class/repository/BookQuery.php
@@ -0,0 +1,29 @@
+<?php
+
+class BookQuery {
+    private $c = "";
+
+    public function bookListup() {
+        $book = array();
+
+        $c = new DbConnector();
+        $connector = $c->connectDb();
+        
+        $sql = "SELECT isbn, title, publisherName, name, roomName, tags FROM book as b, author as a, room as r, publisher as p WHERE b.authorID = 
+        a.authorId and b.roomId = r.roomId and b.publisherId = p.publisherId";
+        
+        $stmh = $connector->prepare($sql);
+        $stmh->execute();
+        
+        if ($stmh->rowCount() < 1) {
+            return $books;    
+        }
+        else {
+            while($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
+                $books[] = array('isbn' => $row['isbn'], 'title' => $row['title'], 'publisherName' => $row['publisherName'], 'name' => $row['name'], 'roomName' => $row['roomName'], 'tags' => $row['tgs']);
+            }
+            return $books;
+        }
+
+    }
+}
