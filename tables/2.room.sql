ef4452f0d691c65719e1e6b0aad6c5d5a2e8e9c2
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-04 08:14:28 +0900

    本の保管場所は変わらないので、テーブル作成時にデータを追加するようにした

--- tables/2.room.sql
+++ tables/2.room.sql
@@ -7,3 +7,8 @@ CREATE TABLE BookManage.room (
 
 CREATE INDEX roomId_index ON BookManage.room(roomId);
 
+INSERT INTO BookManage.room (roomName) VALUES ("書斎　東本棚（右）");
+INSERT INTO BookManage.room (roomName) VALUES ("書斎　東本棚（左）");
+INSERT INTO BookManage.room (roomName) VALUES ("書斎　南本棚");
+INSERT INTO BookManage.room (roomName) VALUES ("書斎　西本棚");
+INSERT INTO BookManage.room (roomName) VALUES ("廊下");
