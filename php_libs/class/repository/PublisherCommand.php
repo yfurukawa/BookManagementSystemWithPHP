baf88ebe7547138ad48fac98dc140ec8c57b5ded
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-11 11:38:56 +0900

    出版社の登録時に出版社名だけでなくISBNの出版社コードも登録するようにした

--- php_libs/class/repository/PublisherCommand.php
+++ php_libs/class/repository/PublisherCommand.php
@@ -5,16 +5,17 @@ require_once('DbConnector.php');
 class PublisherCommand {
     private $c = "";
 
-    public function resisterPublisher($publisherName) {
+    public function resisterPublisher($isbn, $publisherName) {
         $c = new DbConnector();
         $connector = $c->connectDb();
         
         try {
             $connector->beginTransaction();
-            $sql = "INSERT INTO publisher (publisherName) VALUES ( :publisherName )";
+            $sql = "INSERT INTO publisher (publisherName, publisherCode) VALUES ( :publisherName, :publisherCode )";
 
             $stmh = $connector->prepare($sql);
             $stmh->bindValue(':publisherName', $publisherName);
+            $stmh->bindValue(':publisherCode', substr($isbn, 4, 4));
             $stmh->execute();
             $connector->commit();
             return "データを".$stmh->rowCount()."件　登録しました";
