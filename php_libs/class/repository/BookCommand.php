e6241e728f5c30b354108abed36eca11a59c1382
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-23 21:30:35 +0900

    書籍情報登録処理を修正した

--- php_libs/class/repository/BookCommand.php
+++ php_libs/class/repository/BookCommand.php
@@ -11,14 +11,21 @@ class BookCommand {
         
         try {
             $connector->beginTransaction();
-            $sql = "INSERT INTO book (isbn, title, description, publisherId, thumbnail, roomId) VALUES ( :isbn, :authorName )";
+            $sql = "INSERT INTO book (isbn, title, description, publisherId, thumbnail, roomId) VALUES ( :isbn, :title, :description, :publisherId, :thumbnail, :roomId )";
 
             $stmh = $connector->prepare($sql);
-            $stmh->bindValue(':isbn', $isbn);
-            foreach($authorNames as $authorName) {
+            $stmh->bindValue(':isbn', $bookInformation->getIsbn());
+            $stmh->bindValue(':title', $bookInformation->getTitle());
+            $stmh->bindValue(':description', $bookInformation->getDescription());
+            $stmh->bindValue(':publisherId', $bookInformation->getPublisherId());
+            $stmh->bindValue(':thumbnail', $bookInformation->getThumbnail());
+            $stmh->bindValue(':roomId', $bookInformation->getRoomId());
+            $stmh->execute();
+/*            foreach($authorNames as $authorName) {
                 $stmh->bindValue(':authorName', $authorName);
                 $stmh->execute();
             }
+            */
             $connector->commit();
             return "データを".$stmh->rowCount()."件　登録しました";
         }
