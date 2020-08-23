630706056ce8a2983ff861b0b69fa78757b3846e
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-23 20:43:04 +0900

    書籍情報のDAOを追加

---
+++ BookInformation.php
@@ -0,0 +1,50 @@
+<?php
+
+class BookInformation {
+    private $isbn;
+    private $title;
+    private $description;
+    private $authors;
+    private $publisherId;
+    private $thumbnail;
+    private $roomId;
+
+    function __construct($isbn, $title, $description, $publisherId, $authors, $publisherId, $thumbnail, $roomId) {
+        $this->isbn = $isbn;
+        $this->title = $title;
+        $this->description = $description;
+        $this->publisherId = $publisherId;
+        $this->authors = $authors;
+        $this->publisherId = $publisherId;
+        $this->thumbnail = $thumbnail;
+        $this->roomId = $roomId;
+    }
+
+    function getIsbn() {
+        return $this->isbn;
+    }
+
+    function getTitle() {
+        return $this->title;
+    }
+
+    function getDescription() {
+        return $this->description;
+    }
+
+    function getAuthors() {
+        return $this->authors;
+    }
+
+    function getPublisherId() {
+        return $this->publisherId;
+    }
+
+    function getThumbnail() {
+        return $this->thumbnail;
+    }
+
+    function getRoomId() {
+        return $this->roomId;
+    }
+}
