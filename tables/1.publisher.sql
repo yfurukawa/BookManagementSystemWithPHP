93a3be0bf9253ac91e14fe33f655d854d06a9cb4
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-30 19:40:58 +0900

    ISBNの出版社コードは4桁とは決まっていないので可変フィールドに変更した

--- tables/1.publisher.sql
+++ tables/1.publisher.sql
@@ -3,11 +3,12 @@ DROP TABLE BookManage.publisher;
 CREATE TABLE BookManage.publisher (
   publisherId SMALLINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   publisherName varchar(100),
-  publisherCode char(4)
+  publisherCode varchar(7)
 );
 
 CREATE INDEX publisherId_index ON BookManage.publisher(publisherId);
 
 INSERT INTO publisher (publisherName, publisherCode) VALUE ("翔泳社", "7981");
 INSERT INTO publisher (publisherName, publisherCode) VALUE ("マイナビ", "8399");
-INSERT INTO publisher (publisherName, publisherCode) VALUE ("O'REILLY", "8731");
+INSERT INTO publisher (publisherName, publisherCode) VALUE ("O'REILLY", "87311");
+INSERT INTO publisher (publisherName, publisherCode) VALUE ("技術評論社", "297");
