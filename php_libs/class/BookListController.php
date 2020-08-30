c1cf4db2fa9fda943f2b540a5587ded6c49ba800
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-30 20:49:24 +0900

    書籍の一覧表示処理を修正

--- php_libs/class/BookListController.php
+++ php_libs/class/BookListController.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('repository/PublisherQuery.php');
+require_once('/var/www/php_libs/class/repository/PublisherQuery.php');
 class BookListController {
     private $c = "";
 
@@ -9,9 +9,11 @@ class BookListController {
         $c = new DbConnector();
         $connector = $c->connectDb();
         
-        $sql = "SELECT isbn, title, publisherName, name, roomName FROM book as b, author as a, room as r, publisher as p WHERE b.authorID = 
-        a.authorId and b.roomId = r.roomId and b.publisherId = p.publisherId";
+        //$sql = "SELECT b.isbn, title, description, publisherName, authorName, thumbnail, roomName FROM book as b, author as a, room as r, publisher as p WHERE b.isbn = 
+        //a.isbn and b.roomId = r.roomId and b.publisherId = p.publisherId";
         
+        $sql = "SELECT isbn, title, description, thumbnail From book";
+
         $stmh = $connector->prepare($sql);
         $stmh->execute();
         
@@ -20,9 +22,9 @@ class BookListController {
         }
         else {
             while($row = $stmh->fetch(PDO::FETCH_ASSOC)) {
-                $pubs = $pubs."<tr><td>".$row['title']."</td><td>".$row['isbn']."</td><td>".$row['publisherName']."</td><td>".$row['name']."</td><td>".$row['roomName']."</td></tr>";
+                $pubs = $pubs."<tr><td>".$row['title']."</td><td>".$row['isbn']."</td><td>".$row['description'].'</td><td><img src="'.$row['thumbnail'].'"></td></tr>';
             }
-            $contents = '<table border="2"><tbody><tr><th>書籍名称</th><th>ISBN</th><th>出版社</th><th>著者</th><th>保管場所</th></tr>';
+            $contents = '<table border="2"><tbody><tr><th>書籍名称</th><th>ISBN</th><th>説明</th><th></th></tr>';
             $contents = $contents.$pubs;
             $contents = $contents."</tbody></table>";
             print $contents;
