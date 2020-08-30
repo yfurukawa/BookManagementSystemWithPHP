2199dea88e57027a1843bc0fdb38dedfa41d45ae
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-08-30 16:58:40 +0900

    修正

--- php_libs/class/Isbn.php
+++ php_libs/class/Isbn.php
@@ -1,7 +1,10 @@
 <?php
 
+require_once('/var/www/php_libs/class/repository/BookQuery.php');
+
 class Isbn {
-    private $isbn = "";
+    private $isbn13 = "";
+    private $prefix = "";
     private $groupCode = "";
     private $publisherCode = "";
     private $issueCode = "";
@@ -12,7 +15,8 @@ class Isbn {
      * @param $isbn 接頭コードなしのISBN13
      */
     function __construct($isbn) {
-        $this->isbn = '978-'.$isbn;
+        $this->isbn13 = '978-'.$isbn;
+        $this->parseIsbn();
     }
 
     /** Webページから渡されるISBN番号は、接頭記号が含まれていないのと
@@ -21,7 +25,24 @@ class Isbn {
      * @param $isbn ISBN13番号
      * @return なし
     */
-    function parseIsbn($isbn) {
-        list($groupCode, $publisherCode, $issueCode, $checkdigit) = explode('-', $this->isbn);
+    function parseIsbn() {
+        list($this->prefix, $this->groupCode, $this->publisherCode, $this->issueCode, $this->checkdigit) = explode('-', $this->isbn13);
+    }
+
+    /** ISBN13を返す
+     * @param なし
+     * @return ISBN13
+     */
+    public function toIsbn13() {
+        return $this->prefix.$this->groupCode.$this->publisherCode.$this->issueCode.$this->checkdigit;
+    }
+
+    /** 書籍の登録有無を確認する
+     * @param なし
+     * @return 問い合わせ結果（登録済み：true、未登録：false）
+     */
+    function isExistBook() {
+        $query = new BookQuery();
+        return $query->isExist($this->isbn13);
     }
 }
