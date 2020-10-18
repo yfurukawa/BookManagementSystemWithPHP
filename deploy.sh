40383053ee90109a55376d15c3a270e2ca5ab337
Yoshihiro Furukawa <yo4.furukawa@gmail.com>
2020-10-18 23:11:40 +0900

    デプロイファイルにcssを追加

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 mkdir -p /var/www/html/BM
 mkdir -p /var/www/php_libs/class/repository
+mkdir -p /var/www/html/BM/css
 cp index.html /var/www/html/BM/
+cp css/* /var/www/html/BM/css
 cp *.php /var/www/html/BM/
 cp -R php_libs/class/* /var/www/php_libs/class/
 cp php_libs/init.php /var/www/php_libs/
